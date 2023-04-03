bf68faabab936f0bfd184e5ae10c2110b609bf04
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-04-03 19:20:30 +0200

    [Add] Function to create card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+
+const containerCard = document.querySelector(".container-card")
+console.log(containerCard)
+
 const TEAM = [
   {
     name : "Wayne Barnett",
@@ -31,17 +35,58 @@ const TEAM = [
   },
 ]
 
+
+
+// ======= Actions =========
+
 for (let member of TEAM){
+  const card = generateCard()
+  generateContent(card)
+  
 
   console.log('-------------------------');
   console.log('LOG di tutto l\'oggetto --->', member)
 
+
   for (let key in member){
     console.log("KEY più VALORE --->",key,":", member[key])
   }
-}
-
-
-function generateCard(){
   
 }
+
+
+
+
+
+
+
+// ======= FUNZIONI ========= 
+
+function generateCard(){
+
+  const card = document.createElement("div")
+  card.classList.add("card")
+  containerCard.appendChild(card)
+
+  return card
+}
+
+function generateContent(card){
+
+  const photo = document.createElement("div")
+  photo.classList.add("photo")
+  card.appendChild(photo);
+
+  const description = document.createElement("div")
+  description.classList.add("description")
+  card.appendChild(description)
+}
+
+
+
+// const photo = document.crateElemeent("div")
+// photo.classList.add("photo")
+// card.appendChild(photo)
+
+// const description = document.crateElemeent("div")
+// description.classList.add("description")
