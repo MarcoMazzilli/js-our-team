618a0189912017bd2bb8dc6a3b6c3a31df04fa26
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-04-03 15:12:45 +0200

    [new] ciclo per leggere l'array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log('hello world!')
+const TEAM = [
+  {
+    name : "Wayne Barnett",
+    role : "Founder & CEO",
+    image : "wayne-barnett-founder-ceo.jpg",
+  }
+]
+
+for (let member of TEAM){
+
+  console.log('LOG di tutto l\'oggetto --->', member)
+  console.log('-------------------------');
+
+  for (let key in member){
+    console.log("KEY più VALORE --->",key,":", member[key])
+    console.log('-------------------------');
+  }
+}
